858a4a762a2b1815b0dfd44deb3054625dfcaac3
Taiabur Rahman <taiaburbd@gmail.com>
2023-10-28 02:25:26 +0200

    update README.md

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import datetime
+import sqlite3
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 @app.route('/')
 def hello():
-    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts, utc_dt=datetime.datetime.utcnow())
 @app.route('/about/')
 def about():
     return render_template('about.html')
